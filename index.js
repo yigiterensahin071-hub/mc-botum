8a820df458a749085d8b16cd04215aabc446d1f1
yigiterensahin071-hub <yigiterensahin071@gmail.com>
2026-09-15 05:21:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,25 +11,40 @@ function createBot() {
   bot.on('spawn', () => {
     bot.setControlState('jump', true);
     setTimeout(() => bot.setControlState('jump', false), 500);
-
     bot.chat('/register Sifre12345 Sifre12345');
     
     setTimeout(() => {
       bot.chat('/login Sifre12345');
-      
       setTimeout(() => {
         bot.setControlState('jump', true);
         setTimeout(() => bot.setControlState('jump', false), 500);
         bot.chat('/skyblock');
-        
         setTimeout(() => {
           bot.setControlState('jump', true);
           setTimeout(() => bot.setControlState('jump', false), 500);
           bot.chat('/tpa Schxy');
         }, 8000);
-        
       }, 3000);
     }, 2000);
+  });
+
+  // BURASI YENİ EKLENDİ - SOHBET DİNLEYİCİ
+  bot.on('message', (message) => {
+    const msg = message.toString();
+    
+    // Oyun içinden sadece içinde "Schxy" geçen cümleleri (yani seni) dikkate alır
+    if (msg.includes('Schxy')) {
+      if (msg.includes('!gel')) {
+        bot.chat('/tpa Schxy');
+      }
+      if (msg.includes('!zıpla')) {
+        bot.setControlState('jump', true);
+        setTimeout(() => bot.setControlState('jump', false), 1500);
+      }
+      if (msg.includes('!kabul')) {
+        bot.chat('/tpaccept');
+      }
+    }
   });
 
   bot.on('end', () => setTimeout(createBot, 10000));
